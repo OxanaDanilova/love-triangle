26f1f79d67adf2d031b104433fb02a43afebfdbd
OxanaDanilova <oxanadanilova11@gmail.com>
2019-03-15 21:03:24 +0100

    feat: Resolve task 'Love-triangle'

--- src/index.js
+++ src/index.js
@@ -3,5 +3,26 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count=0;
+  for (let i=0; i<preferences.length;) {
+    if (preferences[i]!=''){
+      if (preferences[i]!=i) {
+    let a=preferences[i];
+    if (a<=preferences.length) {
+      if (preferences[a-1]!=a) {
+    let b=preferences[a-1];
+    if (b<=preferences.length){
+      if (preferences[b-1]!=b) {
+    let c=preferences[b-1];
+    if (preferences[c-1]===a) {
+      count++;
+      preferences[a-1]='';
+      preferences[b-1]='';
+      preferences[c-1]='';
+
+     }}}}}}} i++;
+  }
+  return count;
 };
+
+
